44229da081c19eec10c5c308c7178562da0a5ca5
marcushaddon <haddon.marcus@gmail.com>
2016-11-17 17:57:03 -0600

    Add FB redirect for production

--- app/common/fbAuthFactory.js
+++ app/common/fbAuthFactory.js
@@ -1,7 +1,8 @@
 crate.factory('fbAuthFactory', function($http, $location, $window){
   return {
     loginEndpoint: 'https://www.facebook.com/v2.8/dialog/oauth?client_id=1780591808825823&redirect_uri=',
-    loginRedirectUri: 'http://localhost:3000/fbLogin.html',
+    // loginRedirectUri: 'http://localhost:3000/fbLogin.html',
+    loginRedirectUri: 'https://crate-test.herokuapp.com/fbLogin.html',
     loginRedirect: function() {
       $window.location.href = encodeURI(this.loginEndpoint + this.loginRedirectUri + '&response_type=token');
     }
