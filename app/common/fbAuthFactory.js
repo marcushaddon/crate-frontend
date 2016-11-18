d23921f54d9d7532b82c3534a4907903c6324db8
marcushaddon <haddon.marcus@gmail.com>
2016-11-18 17:26:07 -0600

    Remove port from FB redirect URI

--- app/common/fbAuthFactory.js
+++ app/common/fbAuthFactory.js
@@ -3,7 +3,7 @@ crate.factory('fbAuthFactory', function($http, $location, $window){
     loginEndpoint: 'https://www.facebook.com/v2.8/dialog/oauth?client_id=1780591808825823&redirect_uri=',
     // loginRedirectUri: 'http://localhost:3000/fbLogin.html',
     // loginRedirectUri: 'https://cratebeta.herokuapp.com/fbLogin.html',
-    loginRedirectUri: $location.protocol() + '://' + $location.host() + ':' + $location.port() + '/fbLogin.html',
+    loginRedirectUri: $location.protocol() + '://' + $location.host() + '/fbLogin.html',
     loginRedirect: function() {
       $window.location.href = encodeURI(this.loginEndpoint + this.loginRedirectUri + '&response_type=token');
     }
