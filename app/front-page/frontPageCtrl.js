79e118c8c3d97d9c76bd526b020efc0216400ad3
marcushaddon <haddon.marcus@gmail.com>
2016-11-20 12:14:55 -0600

    Add pagination to front-page tags

--- app/front-page/frontPageCtrl.js
+++ app/front-page/frontPageCtrl.js
@@ -1,9 +1,27 @@
 crate.controller('FrontPageCtrl', function($scope, $location, config, tagFactory, albumFactory, playlistFactory) {
+  $scope.tagPageSize = 10;
   $scope.latestAlbums    = [];
   $scope.latestPlaylists = [];
+  $scope.topTags = [];
+  $scope.tagsFinished = false;
+  $scope.tagsAreDone = function() { return $scope.tagsFinished; };
   $scope.today = new Date();
 
   $scope.albumImgPlaceHolder = config.albumImgPlaceHolder;
+
+  $scope.moreTags = function() {
+    var currentPage = $scope.topTags.length / $scope.tagPageSize;
+    console.log(currentPage);
+    tagFactory.getTopTags(currentPage + 1, $scope.tagPageSize)
+    .then(function(response){
+      if (response.data.length < $scope.tagPageSize) {
+        $scope.tagsFinished = true;
+      } else {
+        $scope.topTags = $scope.topTags.concat(response.data);
+      }
+
+    });
+  };
 
   $scope.init = function() {
     albumFactory.getLatestAlbums(4,0).then(function(response){
@@ -14,7 +32,7 @@ crate.controller('FrontPageCtrl', function($scope, $location, config, tagFactory
       $scope.latestPlaylists = response.data;
     });
 
-    tagFactory.getTopTags(1, 30)
+    tagFactory.getTopTags(1, $scope.tagPageSize)
     .then(function(response){
       $scope.topTags = response.data;
     })
