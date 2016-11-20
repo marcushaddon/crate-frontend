27dd35ceebd888776658c0673cab1c1c8553f714
marcushaddon <haddon.marcus@gmail.com>
2016-11-20 13:10:07 -0600

    Fix local FB redirect Uri

--- app/common/fbAuthFactory.js
+++ app/common/fbAuthFactory.js
@@ -1,11 +1,18 @@
 crate.factory('fbAuthFactory', function($http, $location, $window){
   return {
+    getRedirectUri: function() {
+      if ($location.host() == 'localhost') {
+        return 'http://localhost:3000/fbLogin.html';
+      } else {
+        return $location.protocol() + '://' + $location.host() + '/fbLogin.html';
+      }
+    },
     loginEndpoint: 'https://www.facebook.com/v2.8/dialog/oauth?client_id=1780591808825823&redirect_uri=',
     // loginRedirectUri: 'http://localhost:3000/fbLogin.html',
     // loginRedirectUri: 'https://cratebeta.herokuapp.com/fbLogin.html',
     loginRedirectUri: $location.protocol() + '://' + $location.host() + '/fbLogin.html',
     loginRedirect: function() {
-      $window.location.href = encodeURI(this.loginEndpoint + this.loginRedirectUri + '&response_type=token');
+      $window.location.href = encodeURI(this.loginEndpoint + this.getRedirectUri() + '&response_type=token');
     }
   };
 })
