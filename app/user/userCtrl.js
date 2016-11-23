e0cccc4c6fbdde31b92f160dd37adedb8e459c84
marcushaddon <haddon.marcus@gmail.com>
2016-11-23 09:26:17 -0600

    [BUGFIX] Fix Navbar on user profile, stray angle bracket on song item

--- app/user/userCtrl.js
+++ app/user/userCtrl.js
@@ -1,44 +1,43 @@
 crate.controller('userCtrl', function($scope, $routeParams, $location, config, user, playlistFactory, albumFactory, messenger){
-  $scope.user = {};
+  $scope.userProfile = {};
   $scope.playlists = [];
   $scope.albums = [];
-  $scope.user = user;
   $scope.viewing = 'tracks';
   $scope.setView = function(view) {
     $scope.viewing = view;
   }
   $scope.init = function() {
     if (!user.isLoggedIn()) $location.path('/');
-    var userId = $routeParams.id;
+    var userProfileId = $routeParams.id;
 
-    user.getUser(userId)
+    user.getUser(userProfileId)
     .then(function(response) {
-      $scope.user = response.data;
+      $scope.userProfile = response.data;
     }, function(response) {
       messenger.show(response.data);
     });
 
-    // Get playlists by userId
+    // Get playlists by userProfileId
     user.getCratePlaylists()
     .then(function(response) {
       $scope.playlists = response.data;
     });
 
-    // Get albums discovered by user
-    albumFactory.getAlbumsByUserId(userId)
+    // Get albums discovered by userProfile
+    albumFactory.getAlbumsByUserId(userProfileId)
     .then(function(response){
       $scope.discoveries = response.data;
     }, function(response){
       messenger.show(response.data);
     });
 
-    // Get albums liked by user
+    // Get albums liked by userProfile
     user.getCrateAlbums()
     .then(function(response){
       $scope.albums = response.data;
     });
 
-    // Get tracks liked by user
+    // Get tracks liked by userProfile
     user.getCrateTracks()
     .then(function(response){
       $scope.crateTracks = response.data;
@@ -53,6 +52,6 @@ crate.controller('userCtrl', function($scope, $routeParams, $location, config, u
 
   };
 
-  $scope.userImgPlaceholder = config.userImgPlaceholder;
+  $scope.userProfileImgPlaceholder = config.userImgPlaceholder;
   $scope.albumImgPlaceHolder = config.albumImgPlaceHolder;
 });
