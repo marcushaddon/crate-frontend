0a6f58f7f453a3adb8b6fd58ed5fdadc04aab184
marcushaddon <haddon.marcus@gmail.com>
2017-01-23 16:06:58 -0600

    [CLEANUP] Remove redirect to front-page if logged in logic from loginController

--- app/login/loginController.js
+++ app/login/loginController.js
@@ -1,15 +1,15 @@
 crate.controller('login', function($scope, $location, $window, fbAuthFactory, user, authTokenFactory){
 
   $scope.init = function() {
-    var token = authTokenFactory.getToken();
-    if (token) {
-      user.refreshUser()
-      .then(function(response){
-        user.info = response.data;
-      });
-
-      $location.path('/front-page');
-    }
+    // var token = authTokenFactory.getToken();
+    // if (token) {
+    //   user.refreshUser()
+    //   .then(function(response){
+    //     user.info = response.data;
+    //   });
+    //
+    //   $location.path('/front-page');
+    // }
   };
 
   $scope.fbLogin = function() {
