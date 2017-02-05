fcd3eddfb99964e845fd69db7bbf1a179207e57d
marcushaddon <haddon.marcus@gmail.com>
2017-02-05 12:10:18 -0600

    [FEATURE] Enable FB Auth login for extension

--- app/login/loginController.js
+++ app/login/loginController.js
@@ -1,4 +1,4 @@
-crate.controller('login', function($scope, $http, $location, $window, angularConfig, messenger, fbAuthFactory, user, authTokenFactory){
+var webLoginController = function($scope, $http, $location, $window, angularConfig, messenger, fbAuthFactory, user, authTokenFactory){
 
   $scope.init = function() {
     $scope.context = angularConfig.context;
@@ -28,4 +28,60 @@ crate.controller('login', function($scope, $http, $location, $window, angularCon
     });
   };
 
-});
+};
+
+var extensionLoginController = function($scope, angularConfig, $http, $location, $window) {
+
+  $scope.sendRequest = function(message, context, callback) {
+      var port = chrome.extension.connect({
+            name: "Sample Communication"
+       });
+       port.onMessage.addListener(function(msg) {
+
+          callback(msg, context);
+        });
+       port.postMessage(message);
+
+    };
+
+    $scope.fbLogin = function() {
+      // var msg = {};
+      // msg.login = true;
+      // $scope.sendRequest(msg, this, function(msg) {
+      //   console.log(msg);
+      // });
+
+      chrome.identity.launchWebAuthFlow(
+        {'url': 'https://www.facebook.com/v2.8/dialog/oauth?client_id=1780591808825823&redirect_uri=https://bknaoleceflaampejemhijmfieojgcie.chromiumapp.org/provider_cb&response_type=token',
+        'interactive': true},
+        function(redirect_url) {
+          // This should be replaced
+          var startIndex = redirect_url.indexOf("access_token=");
+          var endIndex = redirect_url.indexOf("expires");
+          var token = redirect_url.slice(startIndex, endIndex - 1);
+          $http({
+            method: "GET",
+            url: angularConfig[angularConfig.environment] + "/auth/extension/facebook/callback?" + token
+          })
+          .then(function(response) {
+            $location.path('/');
+          });
+        });
+
+    };
+
+};
+
+// https://bknaoleceflaampejemhijmfieojgcie.chromiumapp.org/provider_cb?#acces…OgNhmlvxdLZAbrDNXu78KVxJR2BywBuxa6ZC3ZBDiu3SVc1vZBFa8vLoZD&expires_in=4292
+
+// angularConfig.environment == 'web'
+var loginController;
+if (angularConfig.environment == 'web') {
+  loginController = webLoginController;
+  console.log("Using web login");
+} else {
+  loginController = extensionLoginController;
+  console.log("Using extension login");
+}
+
+crate.controller('login', loginController);
